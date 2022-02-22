802a0dfc404095b0f020ee1efb8d8592cbc54410
gumingWu <15626185054@163.com>
2022-02-23 00:05:51 +0800

    初步完成功能

--- esm/index.js
+++ esm/index.js
@@ -1,13 +1,36 @@
 import { createUnplugin } from "unplugin";
+import { parse, compileScript } from "@vue/compiler-sfc";
+import { MagicString } from "@vue/compiler-sfc";
 export const unplugin = createUnplugin((options) => {
     return {
         name: "vue-setup-name-plugin",
+        enforce: "pre",
         transformInclude(id) {
             return id.endsWith(".vue");
         },
-        transform(code) {
-            console.log(code);
-            return code.replace(/<template>/, `<template><div>Injected</div>`);
+        transform(code, id) {
+            let s;
+            const str = () => s || (s = new MagicString(code));
+            const { descriptor } = parse(code);
+            if (!descriptor.script && descriptor.scriptSetup) {
+                const result = compileScript(descriptor, { id });
+                const name = result.attrs.name;
+                const lang = result.attrs.lang;
+                console.log(name, lang);
+                if (name) {
+                    str().appendLeft(0, `<script ${lang ? `lang="${lang}"` : ""}>
+    import { defineComponent } from 'vue'
+    export default defineComponent({
+      name: '${name}',
+    })
+    </script>\n`);
+                }
+                return {
+                    map: str().generateMap(),
+                    code: str().toString(),
+                };
+            }
+            return null;
         },
     };
 });
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,43 @@
 import { createUnplugin } from "unplugin";
+import { parse, compileScript } from "@vue/compiler-sfc";
+import { MagicString } from "@vue/compiler-sfc";
 
 export const unplugin = createUnplugin((options) => {
   return {
     name: "vue-setup-name-plugin",
+    enforce: "pre",
     transformInclude(id: string) {
       return id.endsWith(".vue");
     },
-    transform(code: string) {
-      console.log(code);
-      return code.replace(/<template>/, `<template><div>Injected</div>`);
+    transform(code: string, id: string) {
+      let s: MagicString | undefined;
+      const str = () => s || (s = new MagicString(code));
+      const { descriptor } = parse(code);
+
+      if (!descriptor.script && descriptor.scriptSetup) {
+        const result = compileScript(descriptor, { id });
+        const name = result.attrs.name;
+        const lang = result.attrs.lang;
+        console.log(name, lang);
+
+        if (name) {
+          str().appendLeft(
+            0,
+            `<script ${lang ? `lang="${lang}"` : ""}>
+    import { defineComponent } from 'vue'
+    export default defineComponent({
+      name: '${name}',
+    })
+    </script>\n`
+          );
+        }
+        return {
+          map: str().generateMap(),
+          code: str().toString(),
+        };
+      }
+
+      return null;
     },
   };
 });
