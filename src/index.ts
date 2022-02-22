4e7acac8f64d8348b6ffa7ef56f6ae580c4d2e77
gumingWu <15626185054@163.com>
2022-02-22 13:15:18 +0800

    发布尝试

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,8 @@ export const unplugin = createUnplugin((options) => {
     },
   };
 });
+
+export const vitePlugin = unplugin.vite;
+export const rollupPlugin = unplugin.rollup;
+export const webpackPlugin = unplugin.webpack;
+export const esbuildPlugin = unplugin.esbuild;
